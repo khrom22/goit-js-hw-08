2f8eca442c8331a5afde2799f3bf70c23edb46ac
Roman Khomyn <107980144+khrom22@users.noreply.github.com>
2023-11-17 22:31:00 +0200

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const list = document.querySelector('.gallery')
+list.insertAdjacentHTML('beforeend', createMarkup())
+
+function createMarkup(galeryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src=${preview} alt="${description}" />
+   </a>
+</li>`
+    ).join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+
+});
